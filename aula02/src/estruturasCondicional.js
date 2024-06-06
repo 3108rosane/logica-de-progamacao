dc19f7f6d8e8e1d2126554b9a2f7327babdc8ea1
3108rosane <rosanemaia3108@gmail.com>
2024-06-05 21:47:31 -0300

    aula 05/06

---
+++ aula02/src/estruturasCondicional.js
@@ -0,0 +1,50 @@
+//IF/ELSE
+var  valor = 10;
+if(valor > 10){
+    console.log("simmmm")
+
+}else   {
+    console.log("nãoooo")
+
+}
+
+var numero = 1;
+
+if(numero > 0){
+    console.log("numero positivo...");
+} else if (numero < 0){
+    console.log("numero negativo...");
+
+}else {
+    console.log(" o numero é zero.");
+
+}
+//
+
+var x = 10;
+if (x < 100){ 
+    console.log("menor que mil");
+
+}
+if (x < 1000){
+    console.log("menor que mil");
+}   
+//
+
+var codigoErro = leia.questionInt("DIGITE O CODIGO DE ERRO");
+switch (codigoErro){
+    case 400 : 
+    case 401 :
+    case 404 :
+        console.log("HTTP CODE - ERRO DE APLICAÇÃO")
+        break;
+
+    case 500:
+    case 501:
+    case 502:
+        console.log("HTTP CODE - ERRO DE SERVIDOR")
+        break; 
+    default:
+        console.log("NÃO É ERRO")
+        break;
+}
---
+++ aula02/src/index.js
@@ -0,0 +1,31 @@
+import leia from "readline-sync"
+  
+ 
+var valor1 = leia.questionFloat ("DIGITE O VALOR 1");
+
+var valor2 = leia.questionFloat("DIGITE O VALOR 2");
+
+var valor3 = leia.questionFloat("DIGITE O VALOR 3");
+
+var valor4 = leia.questionFloat("DIGITE O VALOR 4");
+
+var valor5 = leia.questionFloat("DIGITE O VALOR 5");
+
+var soma = valor1 + valor2 + valor3 + valor4 + valor5
+var valorLiquido = valorBruto - (valorBruto * 0.25);
+
+console.log("TOTAL"+ total)
+console.log("VALOR LIQUIDO R$" + Soma.toFixed(2));
+console.log("DESCONTO 25% R$" + desconto.toFixed(2));
+console.log("ARRECADADO R$" + arrecadado.toFixed(2));
+
+
+
+
+
+
+
+
+
+
+
