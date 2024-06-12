1fdc7bf052e34dcaf15d4175d861f22c8a138929
3108rosane <rosanemaia3108@gmail.com>
2024-06-12 19:33:45 -0300

    novas

---
+++ aula02/src/exercicios.js
@@ -0,0 +1,118 @@
+//validação de senha exerc 1
+
+import leia from "readline-sync"
+
+var senha = leia.question("DIGITE A SENHA");
+    if (senha == '1234');{
+    console.log ("acesso permitido");
+}   else {
+    console.log ("acesso negado");
+
+    switch (senha){
+        case "1234":
+            console.log("permitir acesso!!")
+            break;
+        default:
+            console.log("acesso negado");
+
+    }
+}
+
+// dizer se o numero é impar ou par exec 2 
+
+import leia from "readline-sync"
+
+var numero = leia.questionInt(" impar ou par ");
+    if ("numero % 2 == 0:");{
+
+       console.log ("númeroPar");
+    }
+    else{
+        console.log ("número impar");
+    }
+  
+    // numero da placa que pode rodar em SP exerc3
+    //
+
+    import leia from "readline-sync"
+
+    var placa = leia.questionInt("digite ultimo numero da placa");
+    if ("placa 0 === || placa 1" );{
+    console.log ("segunda feira não pode rodar");
+    }elseif("placa final 2 ||  3");{
+        console.log("terça feira não pode rodar");
+    }elseif("placa final 4 || 5");{
+        console.log("quarta feira não pode rodar");
+    }elseif("placa final 6 || 7");{
+        console.log("quinta feira não pode rodar");
+    }elseif("placa final 8 || 9");{
+        console.log("sexta feira não pode rodar");
+    }
+
+    //calcular custo das maçã
+
+    import leia from "readline-sync"
+
+    var quantidade = leia.questionInt("digite a quantidade");
+        if ("quantidade < 12:");{
+            console.log("custo total == quantidade * 0.30");
+        }else("custo total == quantidade * 0.25")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+    9
+    ''
+
+
+
