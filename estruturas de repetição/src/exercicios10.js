86d80dcf622ff589be2b3faefde44a228ee3ebc7
3108rosane <rosanemaia3108@gmail.com>
2024-06-25 21:47:29 -0300

    estruturas de repeticao

--- estruturas de repetição/src/exercicios10.js
+++ estruturas de repetição/src/exercicios10.js
@@ -5,10 +5,12 @@ var alturaValentina = 1.50;
 var alturaJoao = 1.40;
 anos = 0;
 
-while(alturaValentina > alturaJoao){
+while(alturaValentina >= alturaJoao){
     alturaJoao += 0.03;
     alturaValentina += 0.02;
     anos++;
 }
 
+console.log("Valentina:" + alturaValentina.toFixed(2) )
+console.log("joao:" + alturaJoao.toFixed(2))
 console.log("demora" + anos + "para joao maior que valentina")
